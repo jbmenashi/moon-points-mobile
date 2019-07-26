b9c1b1e171da8f9b8890885c399e711f6a6a9ba5
Jacob <jbmenashi@gmail.com>
2019-07-26 12:35:11 -0400

    home page styling

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, YellowBox } from 'react-native';
+import { Button } from 'react-native-elements'
 import { connect } from 'react-redux'
 
 const mapStateToProps = state => {
@@ -12,10 +13,10 @@ class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-         <Text>Welcome to Moon Points!!</Text>
-         <Button title="My Profile" onPress={() => this.props.navigation.navigate('MyProfile')}/>
-         <Button title="Standings" onPress={() => this.props.navigation.navigate('Standings')}/>
-         <Button title="Stats" onPress={() => this.props.navigation.navigate('Stats')}/>
+         <Text style={styles.title}>Moon Points!</Text>
+         <Button style={styles.link} title="My Profile" onPress={() => this.props.navigation.navigate('MyProfile')}/>
+         <Button style={styles.link} title="Standings" onPress={() => this.props.navigation.navigate('Standings')}/>
+         <Button style={styles.link} title="Stats" onPress={() => this.props.navigation.navigate('Stats')}/>
       </View>
     );
   }
@@ -24,10 +25,14 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#7A13E1',
       alignItems: 'center',
       justifyContent: 'center',
    },
+   title: {
+      color: '#F3D83A',
+      fontSize: 30
+   }
 });
 
 export default connect(mapStateToProps)(Home);
